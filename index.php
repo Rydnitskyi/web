a4e2e26e1459b4afbd0dcda02a6c3a0097a82526
IlyaRudnitskyi <kedaq@yandex.ru>
2021-11-07 19:00:02 +0300

    lesson_1

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<?php
+    $h1 = 'H1';
+    $title = 'Title';
+    $year = date('Y');
+?>
+
+<html>
+<head>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h1><?php echo $h1; ?></h1>
+    <span><?php echo $year; ?></span>
+</body>
+</html>
